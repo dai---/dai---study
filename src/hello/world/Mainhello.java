c6105f0cc0cd8ec1ccbca361a7b83e1a285df3c5
Dai Tsushima <dai.tsushima@gmail.com>
2014-04-20 18:12:20 +0900

    いろいろ

---
+++ src/hello/world/Mainhello.java
@@ -0,0 +1,26 @@
+package hello.world;
+
+public class Mainhello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("つしま");
+		System.out.println("28歳");
+		System.out.println("お酒が好きです。");
+		System.out.println("31+31の計算をします");
+		System.out.println(31 + 31);
+		int x;
+		x = 6;
+		System.out.println(35 - 10);
+		System.out.println(-5 * 2);
+		System.out.println(6 * 6 * 3.14);
+		System.out.println(x * x * 3.14);
+		System.out.println( "こたえは" + 64);
+		System.out.println( "3 + 5 のこたえは");
+		int a = 3 + 5;
+		System.out.println(a);
+
+	}
+}
